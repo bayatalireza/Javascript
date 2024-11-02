a2e5119f1a320d278cd311509a985b30560e73f4
alireza <bayatalireza86@gmail.com>
2024-11-02 17:00:06 +0330

    add popup  in first page

--- test1/accordion.js
+++ test1/accordion.js
@@ -1,68 +1,95 @@
-let  accordions = document.querySelectorAll(".accordion")
+let popupBtn = document.querySelector("button.popup-btn");
 
-accordions.forEach(accordion => {
-  Array.from(accordion.children).forEach(wrapper => {
-    
-    if(wrapper.classList.contains("show")){
-      let div = wrapper.querySelector('div')
-      
-      div.style.maxHeight= div.scrollHeight+30+"px";
 
-    }
+if (popupBtn) {
+  var popupWrapper = document.createElement("div");
+  popupWrapper.className = "popup-wrapper";
+  document.body.prepend(popupWrapper);
+}
+popupBtn.addEventListener("click", (e) => {
+  let popup = popupBtn.nextElementSibling;
+
   
-  
-    wrapper.querySelector("span").addEventListener("click", e => {
-      let span = e.target
-      let wrapper = span.parentElement
-      let div = span.nextElementSibling 
-  
-      wrapper.classList.toggle("show")
-      if(wrapper.classList.contains("show")){
-        div.style.maxHeight= div.scrollHeight+30+"px";
-        // console.log(div.scrollHeight + "px");
-        // console.log(div.style.maxheight);
-      }else{
-        div.style.maxHeight = null;
-      }
-      
-      Array.from(accordion.children).forEach(w =>{
-        if(w != wrapper){
-          w.classList.remove("show")
-          w.querySelector('div').style.maxheight = null
-        }
+  popup.classList.add("show");
+  popupWrapper.classList.add("show");
+
+  let closesPopup = document.querySelectorAll(".popup-close");
+  console.log(closesPopup);
+    closesPopup.forEach(pClose => {
+      pClose.addEventListener("click", (e) => {
+        popup.classList.remove("show")
+        popupWrapper.classList.remove("show")
       })
     })
-  })
-})
-let scroll = document.querySelector("button.scroll")
-let scroll2 = document.querySelector("button.scroll2")
+});
+// let popupClosesFunc = (e) => {
+//   popup.classList.remove("show");
+//   popupWrapper.classList.remove("show");
+// };
+
+
+
+let accordions = document.querySelectorAll(".accordion");
+
+accordions.forEach((accordion) => {
+  Array.from(accordion.children).forEach((wrapper) => {
+    if (wrapper.classList.contains("show")) {
+      let div = wrapper.querySelector("div");
+
+      div.style.maxHeight = div.scrollHeight + 30 + "px";
+    }
+
+    wrapper.querySelector("span").addEventListener("click", (e) => {
+      let span = e.target;
+      let wrapper = span.parentElement;
+      let div = span.nextElementSibling;
+
+      wrapper.classList.toggle("show");
+      if (wrapper.classList.contains("show")) {
+        div.style.maxHeight = div.scrollHeight + 30 + "px";
+        // console.log(div.scrollHeight + "px");
+        // console.log(div.style.maxheight);
+      } else {
+        div.style.maxHeight = null;
+      }
+
+      Array.from(accordion.children).forEach((w) => {
+        if (w != wrapper) {
+          w.classList.remove("show");
+          w.querySelector("div").style.maxheight = null;
+        }
+      });
+    });
+  });
+});
+let scroll = document.querySelector("button.scroll");
+let scroll2 = document.querySelector("button.scroll2");
 window.addEventListener("scroll", () => {
-  if(window.scrollY > 500){
-    scroll.classList.add("show")
+  if (window.scrollY > 500) {
+    scroll.classList.add("show");
   } else {
-    scroll.classList.remove("show")
+    scroll.classList.remove("show");
   }
-})
+});
 
 scroll.addEventListener("click", () => {
-  if(scroll.classList.contains("show")){
-
-    window.scrollTo({top: 0, behavior: "smooth"})
+  if (scroll.classList.contains("show")) {
+    window.scrollTo({ top: 0, behavior: "smooth" });
   }
-})
-
+});
 
 window.addEventListener("scroll", () => {
-  if(window.scrollY > 2500){
-    scroll2.classList.add("show")
+  if (window.scrollY > 2500) {
+    scroll2.classList.add("show");
   } else {
-    scroll2.classList.remove("show")
+    scroll2.classList.remove("show");
   }
-})
+});
 
 scroll2.addEventListener("click", () => {
-  if(scroll2.classList.contains("show")){
-
-    document.querySelector("h2#article").scrollIntoView({top:0, behavior: "smooth"})
+  if (scroll2.classList.contains("show")) {
+    document
+      .querySelector("h2#article")
+      .scrollIntoView({ top: 0, behavior: "smooth" });
   }
-})
+});
