6f161d7f1ec84f39fbb9144fe8ffeea01d8f2923
alireza <bayatalireza86@gmail.com>
2024-11-08 18:39:50 +0330

    modify for complete theme mini project

--- js-test2/theme/theme.js
+++ js-test2/theme/theme.js
@@ -1,9 +1,19 @@
 "use strict";
 let themes = document.querySelector(".themes");
+let selection = document.querySelector("#selection")
+let selectedTheme = localStorage.getItem("theme") ? localStorage.getItem("theme") : "dark"
+selection.textContent = `Theme is: ${selectedTheme}`
+document.body.className = selectedTheme;
+
+
+
 Array.from(themes.children).forEach(theme=>{
-      theme.addEventListener("click", e=>{
-            let color = e.target.dataset.color
-            document.body.style.backgroundColor = color;
+      
+      theme.addEventListener("click", e=>{            
+            let color = e.target.dataset.color           
+            document.body.className = color;
+            localStorage.setItem("theme", color)
+            
       })
 })
 
