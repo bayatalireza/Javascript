1b7e4ec5ef0c409da7aecc30efaa35a33e3f24b1
alireza <bayatalireza86@gmail.com>
2024-11-22 00:06:18 +0330

    modify callback file and exercise to async fetch api

--- js-test2/callback/Callback.js
+++ js-test2/callback/Callback.js
@@ -1,38 +1,32 @@
-let myProjects = [
-  {
-    title: "clock",
-    technologies: "JavaScript",
-  },
-  {
-    title: "memory",
-    technologies: "React",
-  },
-];
+let post_url = "https://jsonplaceholder.typicode.com/posts/";
+let user_url = "https://jsonplaceholder.typicode.com/users/";
+let app = document.querySelector("#app");
+// fetch(url)
+//   .then((Response) => Response.json())
+//   .then((data) => console.log(data));
 
-function show() {
-  myProjects.forEach((project) => {
-    console.log(project.title, project.technologies);
+async function getAuthor(url, id) {
+  let response = await fetch(url + id);
+  let author = await response.json();
+  return author;
+}
+
+async function getArticles(url) {
+  let response = await fetch(url);
+  let data = await response.json();
+  data.forEach(async (post) => {
+    post.user = await getAuthor(user_url, post.userId);
+
+    app.innerHTML += `
+                  <article key=${post.title}>
+                        <h1 class='h3'>${post.title}</h1>
+                        <small class="btn btn-secondary">${post.user.name}</small>
+                        <p class='mb-4'>${post.body}</p>
+                  </article>
+                  <hr>
+            `;
   });
 }
-show();
-//promise
-function add() {
-  return new Promise((resolve, reject) => {
-    newProject = {
-      title: "foodBill",
-      technologies: "nodejs",
-    };
-    setTimeout(() => {
-      myProjectsLength = myProjects.length;
-      myProjects.push(newProject);
-      if (myProjects.length == myProjectsLength + 1) {
-        resolve("myProjects added successfully!");
-      } else {
-        reject("Failed to add myProjects!");
-      }
-    }, 500);
-  });
-}
-//then and catch promise
-add().then(data=>console.log(data))
-.catch(error=>console.log(error));
+
+getArticles(post_url);
+
